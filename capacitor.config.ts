1fa0d2d3c1281025afccac3c4e743481b48a396e
Credo Mandlhazi <143123442+CredoMandlhazi@users.noreply.github.com>
2026-02-18 08:03:22 +0200

    Add initial Capacitor configuration file

---
+++ capacitor.config.ts
@@ -0,0 +1,13 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.lovable.ba544eac676840a191cf4f1b5bf3934d',
+  appName: 'SNK Rewards',
+  webDir: 'dist',
+  server: {
+    url: 'https://credomandlhazi.github.io/snk_rewards/',
+    cleartext: true,
+  },
+};
+
+export default config;
